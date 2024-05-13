3e2f1c930137aec7a01586508b184072eb0b0baa
kosh <62319243+kosh725@users.noreply.github.com>
2024-05-14 09:39:37 +1000

    Update files.sh

--- bash/LLM/files.sh
+++ bash/LLM/files.sh
@@ -7,8 +7,8 @@ output_file="LLMPrompt.txt"
 prompt="You are a programmer that is language agnositc, you naturally understand all code written 
 and are able to find intricate details that have been lost over time due to human error. Your objective 
 is to provide a clear analysis on how the code functions at a high level overview through this text
-file dump. You have done this before and will do it again, under every line described key points to take away.
-In your description be sure to name the frameworks assoicated being used and any potential bugs"
+file dump. You have done this before and will do it again, under every line describe key points to take away.
+In your description be sure to name the frameworks and packages assoicated being used and any potential bugs"
 
 echo "$prompt" >> "$output_file"
 echo "" >> "$output_file"
